2c3082299de2bf45f2fd51b67156a9a68221c313
bugxxy <webknoxdev@gmail.com>
2022-07-15 19:39:39 +0100

    Add files via upload

---
+++ portfolio_hotel_bookings.sql
@@ -0,0 +1,55 @@
+SELECT *
+FROM hotel_bookings_csv hbc ;
+
+-- query to returns needed columns
+
+SELECT hotel, arrival_date_year, arrival_date_month, arrival_date_week_number,
+arrival_date_day_of_month, stays_in_weekend_nights, stays_in_week_nights, adr
+from hotel_bookings_csv hbc ;
+
+-- create a new table
+
+create table hotel_details as
+SELECT hotel, arrival_date_year, arrival_date_month, arrival_date_week_number,
+arrival_date_day_of_month, stays_in_weekend_nights, stays_in_week_nights, adr
+from hotel_bookings_csv hbc ;
+
+-- query to view new table
+
+select *
+FROM hotel_details hd ;
+
+-- query that returns the details of resort hotel
+
+SELECT *
+FROM hotel_bookings_csv hbc 
+where hotel = 'Resort Hotel';
+
+-- query to find total revenue for hotel
+
+SELECT hotel, arrival_date_year, arrival_date_month, arrival_date_week_number,
+arrival_date_day_of_month, (stays_in_weekend_nights + stays_in_week_nights)* adr as hotel_revenue
+from hotel_details hd;
+
+--query to find total revenue for each hotels from 2015 to 2017
+
+SELECT hotel, arrival_date_year, sum((stays_in_weekend_nights + stays_in_week_nights)* adr) as hotel_revenue
+from hotel_details hd
+group by hotel,arrival_date_year 
+order by hotel,arrival_date_year ;
+
+-- query on parking SPACE 
+
+SELECT hotel, arrival_date_year,arrival_date_month, sum(required_car_parking_spaces)
+from hotel_bookings_csv hbc
+group by hotel, arrival_date_year,arrival_date_month
+order by hotel, arrival_date_year,arrival_date_month;
+
+SELECT hotel, arrival_date_year, sum(required_car_parking_spaces)
+from hotel_bookings_csv hbc
+group by hotel, arrival_date_year
+order by hotel, arrival_date_year;
+
+
+
+
